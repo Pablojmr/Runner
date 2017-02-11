2ed55bdff2dbb90c57592adda5d45ff536f58e14
Pablojmr <pablojmr03@gmail.com>
2017-02-10 22:03:35 -0500

    tutoria

---
+++ laberinto/Assets/Menu/Script/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CargaNivel(string pNombreNivel)
+    {
+        SceneManager.LoadScene(pNombreNivel);
+    }
+}
---
+++ laberinto/Assets/Tutorial/Script/prueba.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prueba : MonoBehaviour {
+
+    private float VelMov = 5f;
+    private float VelGiro = 20f;
+    private bool avanzar = false;
+    private bool girarIzq = false;
+    private bool girarDer = false;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    private void FixedUpdate()
+    {
+        if (avanzar == true)
+        {
+            transform.Translate(Vector3.forward * VelMov * Time.deltaTime);
+        }
+        if (girarIzq == true)
+        {
+            transform.Rotate (Vector2.up * VelGiro * Time.deltaTime);
+        }if (girarDer == true)
+        {
+            transform.Rotate(Vector2.down * VelGiro * Time.deltaTime);
+        }
+    }
+
+    public void Avanzar()
+    {
+        avanzar = true;
+    }
+
+    public void DejarDeAvanzar()
+    {
+        avanzar = false;
+    }
+
+    public void GirarIzq ()
+    {
+        girarIzq = true;
+    }
+
+    public void GirarDer()
+    {
+        girarDer = true;
+    }
+
+    public void DejarDeGirar ()
+    {
+        girarDer = false;
+        girarIzq = false;
+    }
+
+    // Update is called once per frame
+
+	
+}
